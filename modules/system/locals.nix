73eedc15f39d6cab2054fd7e4c76a8a4792e6e65
MoDevIO <momo.tiltis@gmail.com>
2026-08-13 11:14:48 +0200

    fix: change consoleKeyMap from gb to uk to hide boot error

--- modules/system/locals.nix
+++ modules/system/locals.nix
@@ -1,12 +1,15 @@
 { keyboardLayout, ... }:
 
+let
+  consoleKeyMap = if keyboardLayout == "gb" then "uk" else keyboardLayout;
+in
 {
   services.xserver.xkb = {
     layout = keyboardLayout;
     variant = "";
   };
 
-  console.keyMap = keyboardLayout;
+  console.keyMap = consoleKeyMap;
   console.font = null;
 
   i18n.defaultLocale = "en_GB.UTF-8";
