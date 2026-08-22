6825ab93c974f7447672765f07afea62c2a8e6a8
MoDevIO <momo.tiltis@gmail.com>
2026-08-22 20:35:33 +0200

    add opencode-go to opencode config

--- home/opencode/opencode.nix
+++ home/opencode/opencode.nix
@@ -3,13 +3,18 @@
 {
   sops = {
     secrets."openrouter_api_key" = { };
-    templates."openrouter_api_key" = {
+    secrets."opencode_api_key" = { };
+    templates."opencode-auth" = {
       path = "${config.home.homeDirectory}/.local/share/opencode/auth.json";
       content = ''
         {
           "openrouter": {
             "type": "api",
             "key": "${config.sops.placeholder."openrouter_api_key"}"
+          },
+          "opencode-go": {
+            "type": "api",
+            "key": "${config.sops.placeholder."opencode_api_key"}"
           }
         }
       '';
