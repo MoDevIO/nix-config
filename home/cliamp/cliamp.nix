6cc80edc8df5a7cd55c30131779d8722dd2f3268
MoDevIO <momo.tiltis@gmail.com>
2026-08-26 12:08:32 +0200

    fix: add force option to cliamp`s config.toml

--- home/cliamp/cliamp.nix
+++ home/cliamp/cliamp.nix
@@ -4,8 +4,12 @@
   home.packages = [
     pkgs.cliamp
   ];
-  home.file.".config/cliamp/config.toml".text = ''
-    [spotify]
-    client_id = "83e1a2a15d9e42b9bfcd755cb686f7da"
-  '';
+  home.file.".config/cliamp/config.toml" = {
+    text = ''
+      [spotify]
+      enabled = true
+      client_id = "83e1a2a15d9e42b9bfcd755cb686f7da"
+    '';
+    force = true;
+  };
 }
