cf5b31e0e0e2e48449d29a382d015f00cce68dcc
MoDevIO <momo.tiltis@gmail.com>
2026-08-30 12:45:10 +0200

    feat(neomutt): add html view support

--- home/neomutt/neomutt.nix
+++ home/neomutt/neomutt.nix
@@ -4,12 +4,27 @@
 
   sops.secrets.gmail_auth_key = { };
 
+  home.packages = [ pkgs.w3m ];
   programs.neomutt = {
     enable = true;
     editor = "nvim";
     vimKeys = true;
 
+    extraConfig = ''
+      set mailcap_path = "~/.config/neomutt/mailcap"
+      auto_view text/html
+      alternative_order text/plain text/enriched text/html
+
+      bind pager q exit
+      bind browser q exit
+      bind attach q exit
+      bind compose q exit
+      bind index q quit
+    '';
   };
+  xdg.configFile."neomutt/mailcap".text = ''
+    text/html; ${pkgs.w3m}/bin/w3m -I %{charset} -T text/html -dump; copiousoutput
+  '';
 
   accounts.email.accounts."momo.tiltis@gmail.com" = {
     primary = true;
